7fe9154001b8937d10f1ae6c20730d5344516ed3
Butovens Médé <bmede@ucmerced.edu>
2020-09-18 21:39:40 -0700

    4th attemp to rectify issue with duplicate file

--- lab.R
+++ lab.R
@@ -33,12 +33,8 @@ opts_chunk$set(out.width='60%', echo=FALSE, fig.align='center')
 #' 
 #' 1. Navigate to the GitHub repo for this lab assignment, <https://github.com/data-science-methods/lab-w2-git>.  Fork the lab, then clone it to your working machine.  Open the file `lab.R` in a *new* R session.  
 #' 2. Edit `lab.R`.  You don't need to do anything in particular, but they should be trackable changes.  Add and commit the changes to your working machine.  In the commit message, put something like "problem 1 step 2."
-#' ## This is Butovens's Médé addition to the lab for this question 
+#' # This is Butovens Médé's 1st change for the redo of the git lab 
 #' 3. Edit `lab.R` again, making another round of changes.  Add and commit the changes to your working machine. In the commit message, put something like "problem 1 step 3." 
-<<<<<<< HEAD
-#' ## This is Butovens's Médé's second round of change for this question
-=======
->>>>>>> parent of acc703b... problem 1 step 3.
 #' 4. Push the repo back up to GitHub.  
 #' 
 
